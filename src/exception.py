0a48b332c3e038ab47c9089a1d8fc8e9c813974d
keshav kumar singh <kumarkeshavsingh200903@gmail.com>
2024-05-05 12:32:24 +0530

    added general file structure

---
+++ src/exception.py
@@ -0,0 +1,25 @@
+import logging
+import sys
+
+def error_message_details(error, error_details):
+    _, _, exc_tb = error_details.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occurred in python script name [{0}] line no [{1}] error message [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
+    )
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message, error_detail)
+        
+    def __str__(self):
+        return self.error_message
+
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.error("An error occurred", exc_info=True)
+        raise CustomException("An error occurred", sys)
